5ce6ff505e76fdbaeff1a495a436c275c92567bf
Ben Kaufman <ben.kaufman@altigen.com>
2019-03-06 07:04:17 -0800

    From Minilla

---
+++ lib/Zabbix/Discovery/Formatter.pm
@@ -0,0 +1,39 @@
+package Zabbix::Discovery::Formatter;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Zabbix::Discovery::Formatter - It's new $module
+
+=head1 SYNOPSIS
+
+    use Zabbix::Discovery::Formatter;
+
+=head1 DESCRIPTION
+
+Zabbix::Discovery::Formatter is ...
+
+=head1 LICENSE
+
+Copyright (C) Ben Kaufman.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Ben Kaufman E<lt>ben.kaufman@altigen.comE<gt>
+
+=cut
+
